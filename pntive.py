0b5615848ef8fd7f9474cce709e8e952178e9c77
Rohit Rannavre <rohit.rannavre@gmail.com>
2021-01-19 12:25:00 +0530

    added posotive/negative number program

---
+++ pntive.py
@@ -0,0 +1,12 @@
+
+#Program to find out if a given number is a positive or negative number
+
+num = float(input("Enter a number: "))
+print ("")
+if num > 0:
+	print ("Positive number")
+elif num == 0:
+	print ("Zero")
+else:
+	print ("Negative number")
+
